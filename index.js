0c5a7b494bfed854cb4cbd5c2c6e2699046afe53
Brian Ochieng Otieno <brianochieng1989@gmail.com>
2023-09-22 14:28:48 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+const destructivelyAppendCat = () => {
+    cats.push ("Ralph");
+}
+const destructivelyPrependCat = () => {
+    cats.unshift ("Bob");
+}
+
+const destructivelyRemoveLastCat = () => {
+    cats.pop("Garfield");
+}
+
+const destructivelyRemoveFirstCat = () => {
+    cats.shift("Bob");
+}
+const appendCat = () => {
+    return [ ... cats , "Broom"];
+}
+
+const prependCat = () =>{
+    return [ "Arnold" , ... cats];
+}
+
+const removeLastCat = () => {
+    return cats.slice(0,2);
+}
+const removeFirstCat = () => {
+    return cats.slice (1)
+}
